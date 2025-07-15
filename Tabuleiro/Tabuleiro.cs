746e8e8994623e31545fb53c6eb3c5e799aa1ebc
Matheus Souza Silva <matheus.silva@foconet.com.br>
2025-07-14 22:36:51 -0300

    tratando erros com exceptions personalizadas

--- Tabuleiro/Tabuleiro.cs
+++ Tabuleiro/Tabuleiro.cs
@@ -15,15 +15,53 @@ namespace tabuleiro
             pecas = new Peca[linhas, colunas];
         }
 
-        public Peca peca(int linha, int coluna)
+        public Peca Peca(int linha, int coluna)
         {
             return pecas[linha, coluna];
         }
 
+        public bool ExistePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return Peca(pos) != null;
+        }
+
+        public Peca Peca(Posicao pos)
+        {
+            return pecas[pos.linha, pos.coluna];
+        }
+
         public void ColocarPeca(Peca p, Posicao pos)
         {
+            if (ExistePeca(pos))
+            { 
+                throw new TabuleiroException("Já existe uma peça nessa posição!");    
+            }
+
             pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
         }
+
+        public bool PosicaoValida(Posicao pos)
+        {
+            if (pos.linha < 0 ||
+                pos.linha >= linhas ||
+                pos.coluna < 0 ||
+                pos.coluna >= colunas)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void validarPosicao(Posicao pos)
+        {
+            if (!PosicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição inválida!");    
+            }
+        }
+
     }
 }
--- Xadrez/Torre.cs
+++ Xadrez/Torre.cs
@@ -6,6 +6,7 @@ namespace xadrez
     {
         public Torre(Tabuleiro tab, Cor cor) : base(tab, cor)
         {
+            
         }
 
         public override string ToString()
